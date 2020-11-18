273d3d40a4f6e6653fad1101eca12cff052d7445
cooll5 <52448498+cooll5@users.noreply.github.com>
2020-11-19 01:01:57 +0200

    first commit

---
+++ Lab4.5/Lab4.5.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y, R;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "R = "; cin >> R;
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		if ( 
+			(y >= 0 && y <= R && x >= R && x <= -R) || 
+			 ((x * x + y * y) / 2) <= (R * R) &&
+			(y >= -R && y <= x - R && y <= -x -R))
+		   )
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+
+	for (int i = 0; i < 10; i++)
+	{
+		x = 6. * rand() / RAND_MAX - R;
+		y = 6. * rand() / RAND_MAX - R;
+		if (
+			(y >= 0 && y <= R && x >= R && x <= -R) ||
+			((x * x + y * y) / 2 <= (R * R)) &&
+			(y >= -R && y <= x - R && y <= -x - R))
+		   )
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+		else
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+	}
+	return 0;
+}
